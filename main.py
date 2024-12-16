cb8cd4c3ae2483d7aa7cb555a5afba32bffd1e9e
Dotsan <dotsan_20@mail.ru>
2024-12-16 15:26:01 +0300

    Бот

--- main.py
+++ main.py
@@ -2,24 +2,25 @@ import asyncio
 import logging
 import sys
 import os
+import config
 
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-from config import BOT_TOKEN
+from handlers.handlers import router
 
 #инициализация
-bot = Bot(token= BOT_TOKEN, parse_mode=ParseMode.HTML)
-dp = Dispatcher()
+async def main():
+    bot = Bot(token=config.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher()
+    dp.include_router(router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
-#хэндлер на команду /start
-@dp.message(CommandStart())
-async def process_start_command(message: Message):
-    kb = [
-        [types.KeyboardButton(text="1")],
-        [types.KeyboardButton(text="2")]
-    ]
-    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
-    await message.answer(text="Hello!", reply_markup=keyboard)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
