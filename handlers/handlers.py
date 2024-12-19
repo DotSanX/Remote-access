18b3028a3b6181fb9b4aa0979b34fe070152f50c
Dotsan <dotsan_20@mail.ru>
2024-12-19 16:46:20 +0300

    Get ip

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -6,18 +6,31 @@ from aiogram.types import FSInputFile
 
 import os
 import pyautogui
+import requests
 
 router = Router()
-def screenshot():
-    screenshot = pyautogui.screenshot()
-    screenshot.save('screen.png')
 
+#Функции
 def main_menu():
     
     keyboard = ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton(text="/screen", )  ]], resize_keyboard=True, input_field_placeholder="Выберите действие")
-    return keyboard
+        [KeyboardButton(text="/screen", ),KeyboardButton(text="/ip", )]], resize_keyboard=True, input_field_placeholder="Выберите действие")
 
+    return keyboard
+def screenshot():
+    screenshot = pyautogui.screenshot()
+    screenshot.save('screen.png')
+def get_ip_address():
+    try:
+        response = requests.get("https://api.ipify.org?format=json", timeout=5)
+        ip_address = response.json().get("ip")
+        return f"🌐 Текущий IP-адрес: {ip_address}"
+    except requests.RequestException:
+        return "❌ Не удалось получить IP-адрес."
+
+
+
+#Обработчики
 @router.message(Command("start"))
 async def send_welcome(message: types.Message):
     await message.reply("Привет!", reply_markup=main_menu())
@@ -33,6 +46,11 @@ async def handle_screenshot(message: types.Message):
         os.remove('screen.png')
     except Exception as e:
         await message.reply(f"❌ Ошибка при создании скриншота: {e}", parse_mode=None)
+
+@router.message(Command("ip"))
+async def handle_ip_address(message: types.Message):
+    ip_address = get_ip_address()
+    await message.reply(ip_address)
 
 
     
